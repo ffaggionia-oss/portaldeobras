ca77c3b3bc2c2303765fc6f44314c68b95a52ffb
ffaggionia-oss <ffaggioni.a@gmail.com>
2026-07-01 09:47:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwDBUYs_XydVGH-K552ES7RcRfiuPsyQ4AN4COlPONO__edrp1JXxRLwOEnpH_k9DfrDQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwzZFeJGHRV7aLRGYpxW12yJywmuzu6fSUMuzl7HgYxhteTPwICOsteXpQBjPGbjakoTg/exec'
 };
